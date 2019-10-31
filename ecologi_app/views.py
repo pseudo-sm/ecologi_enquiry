5e54676287934af17fbdce7f7ad849fd0ec9227b
pseudo-sm <saswathcommand@gmail.com>
2019-10-31 20:08:49 +0530

    code added to api

--- ecologi_app/views.py
+++ ecologi_app/views.py
@@ -12,7 +12,16 @@ def email_form(request):
     linkedin = request.POST.get("linkedin")
     message = request.POST.get("message")
     subject = request.POST.get("subject")
-    body ="Name : "+name+"\nEmail : "+email+"\n Linkedin : "+linkedin+"\nSubject : "+subject+"\nMessage : "+message
+    code = request.POST.get("code")
+    body ="Name : "+name+"\nEmail : "+email+"\n Linkedin : "+linkedin+"\nSubject : "+subject+"\nMessage : "+message+"\n Code : "+code
     email_msg = EmailMessage("ecologi enquiry", body, settings.EMAIL_HOST_USER, ["ionut.procop.87@gmail.com"])
     email_msg.send(fail_silently=False)
     return JsonResponse(True,safe=False)
+
+def onelife(request):
+
+    messaage = request.POST.get("message")
+    name = request.POST.get("name")
+    email = request.POST.get("email")
+    subject = request.POST.get("subject")
+    email_msg = EmailMessage("ecologi enquiry", messaage, settings.EMAIL_HOST_USER, ["ionut.procop.87@gmail.com"])
